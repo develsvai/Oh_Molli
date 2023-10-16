d4a5ea46db5e2fbd0abcb2f2e2fb4ba1264fc26b
hongyongjae <developsvai5096@gmail.com>
2023-10-16 09:16:21 +0900

    second

--- util/database.js
+++ util/database.js
@@ -6,8 +6,10 @@ let connectDB
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
+
     global._mongo = new MongoClient(url, {
       useUnifiedTopology: true, // 이 옵션을 활성화합니다.
+      
     }).connect()
   }
   connectDB = global._mongo
